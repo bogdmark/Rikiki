beeb9d5df6e65c4f07497e96ef3d6b79eb98ad56
MalustyikAndras <eliakwubme@gmail.com>
2015-12-07 15:14:16 +0100

    Estimate hangolás
    Órára kellett indulnom, ezért még nem teljes. Sok sok tesztelés kell még
    neki :)

--- src/rikiki/Robot.java
+++ src/rikiki/Robot.java
@@ -21,76 +21,17 @@ public class Robot extends Player{
     
     @Override
     public void setCurrentRankForCardsInHand(){
-        int cardsinplay;
-        int spades, diamonds, hearts, clubs;
-        spades = diamonds = hearts = clubs = 0;
-        cardsinplay = cards.size() * this.nrofplayers;
-        for (Card card : cards) { // Beállítjuk melyik típusból mennyi van a kezünkben
-            for(Card card_type : cards){
-                if("spades".equals(card_type)) spades++;
-                if("diamonds".equals(card_type)) diamonds++;
-                if("hearts".equals(card_type)) hearts++;
-                if("clubs".equals(card_type)) clubs++;
-            }
-        /*Ha a kapott lapok között szerepel Ász, vagy adu Ász 
-        vagy adu Király akkor növeljük a CurrentRank értékét*/
-            if (card.getRoundRank() == 12)
-                card.setCurrentRank(0.9);
-            if (card.getRoundRank() == 32 || card.getRoundRank() == 31)
-                card.setCurrentRank(1.0);                
-            if(card.getRoundRank() >= 20) card.setCurrentRank(0.5);
-            
-            for(int i = 0; i < cards.size(); i++){
-        /* Az alapján, hogy melyik típusból mennyi van a kezünkben,
-           növeljük a CurrentRank értékét arányosan. Ha mi kezdünk, akkor nagyobb mértékkel.*/
-                if(round_starter){
-                    if(spades == i){
-                        for(Card card_type : cards)
-                            if(card_type.getType().equals("spades")) card_type.setCurrentRank(i*0.1);
-                    }
-                    if(diamonds == i){
-                        for(Card card_type : cards)
-                            if(card_type.getType().equals("diamonds")) card_type.setCurrentRank(i*0.1);
-                    }
-                    if(hearts == i){
-                        for(Card card_type : cards)
-                            if(card_type.getType().equals("hearts")) card_type.setCurrentRank(i*0.1);
-                    }
-                    if(clubs == i){
-                        for(Card card_type : cards)
-                            if(card_type.getType().equals("clubs")) card_type.setCurrentRank(i*0.1);
-                    }
-                }               
-                else {
-                    if(spades == i && i > 2){
-                        for(Card card_type : cards)
-                            if(card_type.getType().equals("spades")) card_type.setCurrentRank(i*0.08);
-                    }
-                    if(diamonds == i && i > 2){
-                        for(Card card_type : cards)
-                            if(card_type.getType().equals("diamonds")) card_type.setCurrentRank(i*0.08);
-                    }
-                    if(hearts == i && i > 2){
-                        for(Card card_type : cards)
-                            if(card_type.getType().equals("hearts")) card_type.setCurrentRank(i*0.08);
-                    }
-                    if(clubs == i && i > 2){
-                        for(Card card_type : cards)
-                            if(card_type.getType().equals("clubs")) card_type.setCurrentRank(i*0.08);
-                    }
-                }
-            }
-        }
     }
     
     @Override
     public void setEstimate(){
         this.estimate = 0;
         if(type == -1){ //minimalista becslő beállítása
-            for (Card card : cards) { 
+            for (Card card : cards) {
+                if(cards.size() < 9){ 
                 /*Ha a kapott lapok között szerepel Ász, vagy adu Ász 
                     vagy adu Király akkor 1-gyel növeljük a becslést*/
-                if ("A".equals(card.getValue())){
+                if (card.getRoundRank() == 0 && card.getAllTimeRank() == 12){
                     card.toWin = true;
                     this.estimate++;
                 }
@@ -114,7 +55,27 @@ public class Robot extends Player{
                                    this.estimate++;
                                    card.toWin = true;
                                }
-                                   
+                }
+                if(cards.size() > 8 && round_starter){
+                    if (card.getRoundRank() == 0 && (card.getAllTimeRank() >= 10)){
+                    card.toWin = true;
+                    this.estimate++;
+                    }
+                    if (card.getRoundRank() >= 28){
+                    card.toWin = true;
+                    this.estimate++;
+                    }
+                }
+                else if(cards.size() > 8 && !round_starter){
+                    if (card.getRoundRank() == 0 && card.getAllTimeRank() >= 11){
+                        card.toWin = true;
+                        this.estimate++;
+                    }
+                    if (card.getRoundRank() >= 29){
+                        card.toWin = true;
+                        this.estimate++;
+                    }
+                }              
             }
         }
         
@@ -122,15 +83,12 @@ public class Robot extends Player{
             for (Card card : cards) { 
                 /*Ha a kapott lapok között szerepel Ász, Király, vagy adu Ász 
                     vagy adu Király akkor 1-gyel növeljük a becslést*/
-                if ("A".equals(card.getValue())){
+                if(cards.size() < 7){
+                if (card.getRoundRank() == 0 && card.getAllTimeRank() >= 11){
                     this.estimate++;
                     card.toWin = true;
                 }
-                if ("K".equals(card.getValue())){
-                    this.estimate++;
-                    card.toWin = true;
-                }
-                if(card.getRoundRank() == 32 || card.getRoundRank() == 31 || card.getRoundRank() == 30 || card.getRoundRank() == 29){
+                if(card.getRoundRank() >= 29){
                     this.estimate++;
                     card.toWin = true;
                 }
@@ -151,29 +109,32 @@ public class Robot extends Player{
                                    this.estimate++;
                                    card.toWin = true;
                                }
+                }
+                if(cards.size() > 6){
+                    if (card.getRoundRank() == 0 && card.getAllTimeRank() >= 10){
+                    card.toWin = true;
+                    this.estimate++;
+                    }
+                    if (card.getRoundRank() >=28){
+                    card.toWin = true;
+                    this.estimate++;
+                    }
+                }
                                    
             }
-            if(this.getRoundStarter()) this.estimate++;
             //túl extrém becslés csökkentése
-            if(this.estimate > 5) this.estimate = 5;
+            if(this.estimate > cards.size()/2) this.estimate = cards.size()/2;
         }
         if(type == 1){
-            for (Card card : cards) { 
+            for (Card card : cards) {
+                if(cards.size() < 6){
                 /*Ha a kapott lapok között szerepel Ász, Király, Dáma, vagy adu Ász 
                     vagy adu Király akkor 1-gyel növeljük a becslést*/
-                if ("A".equals(card.getValue())){
+                if (card.getRoundRank() == 0 && card.getAllTimeRank() >= 10){
                     this.estimate++;
                     card.toWin = true;
                 }
-                if ("K".equals(card.getValue())){
-                    this.estimate++;
-                    card.toWin = true;
-                }
-                if ("Q".equals(card.getValue())){
-                    this.estimate++;
-                    card.toWin = true;
-                }
-                if(card.getRoundRank() == 32 || card.getRoundRank() == 31 || card.getRoundRank() == 30 || card.getRoundRank() == 29){
+                if(card.getRoundRank() >= 29){
                     this.estimate++;
                     card.toWin = true;
                 }
@@ -193,16 +154,37 @@ public class Robot extends Player{
                                    this.estimate++;
                                    card.toWin = true;
                                }
-                                   
+                }
+                if(cards.size() > 5){
+                    if (card.getRoundRank() == 0 && card.getAllTimeRank() >= 10){
+                    card.toWin = true;
+                    this.estimate++;
+                    }
+                    if (card.getRoundRank() >=28){
+                    card.toWin = true;
+                    this.estimate++;
+                    }
+                    if (card.getRoundRank() == 32) 
+                    for (int i = 0; i < cards.size(); i++)
+                        if (card.getRoundRank() == 31)
+                           for (int j = 0; j < cards.size(); j++)
+                               if (card.getRoundRank() == 30)
+                                   for (int k = 0; k < cards.size(); k++)
+                                       if(card.getRoundRank() == 29){
+                                           this.estimate++;
+                                           card.toWin = true;
+                                        }
+                }
             }
-            if(this.getRoundStarter()) this.estimate++;
         }
+        
         if(type == 2){
             this.estimate = 0;
         }
     }
+}
     
 
     
     
-}
+
