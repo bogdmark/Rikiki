65f1cf1b01b88d3ffe4f6c185699a8824821339c
Mark Bogdan <bogdmark@gmail.com>
2015-12-16 21:12:06 +0100

    ??

--- compile.bat
+++ compile.bat
@@ -4,7 +4,6 @@ rm -r -f build
 mkdir build
 echo Compiling...
 
-set path=%path%;C:\Program Files\Java\jdk1.8.0_65\bin
 
 javac -classpath .\src\ -encoding UTF-8 -d .\build\ .\src\rikiki\Rikiki.java
 @echo on
